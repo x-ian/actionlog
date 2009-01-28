4a12a040826a23f8e456bbd379fea61662a75de5
U-CHRIS\neumann <neumann@chris.(none)>
2009-01-28 19:00:19 +0100

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+set :application, "actionlog"
+set :repository, "/vol/git/actionlog.git" 
+
+set :user, "root"
+set :use_sudo, false
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/home/webuser/capistrano/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+set :scm, :git
+set :deploy_via, :remote_cache
+
+role :app, "actionlog.dynalias.com"
+role :web, "actionlog.dynalias.com"
+role :db,  "actionlog.dynalias.com", :primary => true
