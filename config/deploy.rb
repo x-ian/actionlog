09e727afad71351034b4248b71cd24eac1d36590
U-CHRIS\neumann <neumann@chris.(none)>
2009-02-02 09:51:36 +0100

    db migrations for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :stages, %w(central_development production)
-#set :default_stage, "central_development"
+set :default_stage, "central_development"
 require 'capistrano/ext/multistage'
 
 set :application, "actionlog"
@@ -8,11 +8,6 @@ set :local_repository, "."
 
 set :user, "root"
 set :use_sudo, false
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/home/webuser/capistrano/actionlog"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
