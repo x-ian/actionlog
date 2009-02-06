6f0ec49888651f0e8aa89c2e6cb0931cb9d6979c
U-CHRIS\neumann <neumann@chris.(none)>
2009-02-06 14:46:57 +0100

    added spinner for action_log controller

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var overlayTimeout;
+
+Ajax.Responders.register({
+  onCreate: function() {
+    if($('overlay') && Ajax.activeRequestCount > 0)
+      overlayTimeout = Effect.Appear.delay(0.5, 'overlay', {duration:0.2, queue:'end'});
+  },
+  onComplete: function() {
+    if($('overlay') && Ajax.activeRequestCount == 0) {
+      window.clearTimeout(overlayTimeout);
+      Effect.Fade('overlay', {duration:0.2, queue:'end'});
+    }
+  }
+});
