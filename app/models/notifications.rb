a347eb51e9dbf47ea594b5af2df64c5e2ac1c692
U-CHRIS\neumann <neumann@chris.(none)>
2009-03-11 14:45:44 +0100

    mail and domain name changed

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -3,7 +3,7 @@ class Notifications < ActionMailer::Base
   def daily_digest_dashboard(user, meeting, server_path, meeting_summary, sent_at = Time.now)
     subject    "Daily Digest Dashboard of meeting #{meeting.name} from #{Time.now.to_date}"
     recipients user.email
-    from       'christian.neumann@gmail.com'
+    from       'info@firstprinciplesmanagement.com'
     sent_on    sent_at
     
     body       :user => user, :meeting => meeting, :server_path => server_path, :meeting_summary => meeting_summary
