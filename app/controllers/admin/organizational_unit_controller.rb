c909cc5cf00171ebb90cc3066700ddd7b0a59c16
U-CHRIS\neumann <neumann@chris.(none)>
2009-03-26 18:47:43 +0100

    orgunits respect the org unit of current user

--- app/controllers/admin/organizational_unit_controller.rb
+++ app/controllers/admin/organizational_unit_controller.rb
@@ -10,13 +10,17 @@ class Admin::OrganizationalUnitController < ApplicationController
   layout "admin"
   active_scaffold :organizational_unit do |config|
     config.actions.exclude :nested
-    #config.label = "Customers"
-    #config.columns = [:name, :phone, :company_type, :comments]
+    config.columns = [:parent, :name, :responsible_user, :description]
     config.columns.exclude :lft, :rgt, :meetings, :users
-    #list.sorting = {:name => 'ASC'}
-    #columns[:phone].label = "Phone #"
-    #columns[:phone].description = "(Format: ###-###-####)"
-    config.columns[:parent].ui_type = :select
-    config.columns[:responsible_user].ui_type = :select
+    list.sorting = {:parent => 'ASC'}
+    config.columns[:parent].form_ui = :select
+    config.columns[:description].options = {:rows => 4, :cols => 41}
+    config.columns[:responsible_user].form_ui = :select
+  end
+
+  def conditions_for_collection
+    if current_user.organizational_units != nil && !current_user.organizational_units.empty?
+      ['organizational_units.id IN (?)', OrganizationalUnit.find_all_organizational_units(current_user.organizational_units)]
+    end
   end
 end
