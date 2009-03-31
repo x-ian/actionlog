a38ac561aa516caba29d96775849b3e0cdda6f30
U-CHRIS\neumann <neumann@chris.(none)>
2009-03-31 11:51:04 +0200

    deactivated mail notification of account activiation cause it is sent 3x

--- app/models/account_observer.rb
+++ app/models/account_observer.rb
@@ -1,11 +1,14 @@
 class AccountObserver < ActiveRecord::Observer
   def after_create(account)
-    AccountMailer.deliver_signup_notification(account)
+    if account.state == "pending"
+      AccountMailer.deliver_signup_notification(account)
+    end
   end
 
   def after_save(account)
-    if account.recently_activated? && account.user != nil && account.user.public_user
-      AccountMailer.deliver_activation(account)
-    end
+    # TODO: why is the mail sent 3 times? deactivated for now
+    #if account.recently_activated? && ((account.user != nil && account.user.public_user) || account.user == nil)
+      #AccountMailer.deliver_activation(account)
+    #end
   end
 end
