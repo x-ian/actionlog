d29632964a4f6ecd50ad0bb122f085413451b85d
U-CHRIS\neumann <neumann@chris.(none)>
2009-04-05 14:13:25 +0200

    geolocating

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,8 @@ Rails::Initializer.run do |config|
 
   config.gem "calendar_date_select"
 
+  config.gem "geokit"
+
   config.gem 'mislav-will_paginate', :version => '2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
