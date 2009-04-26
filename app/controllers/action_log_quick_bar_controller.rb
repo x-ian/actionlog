bbc9af1a8150f1890d29f5d46e1bfa046b7bdcad
U-CHRIS\neumann <neumann@chris.(none)>
2009-04-26 09:22:13 +0200

    exception catching for meeting summary

--- app/controllers/action_log_quick_bar_controller.rb
+++ app/controllers/action_log_quick_bar_controller.rb
@@ -3,8 +3,13 @@ class ActionLogQuickBarController < ApplicationController
   before_filter :login_required
 
   def update_meeting_summary
-    s = create_meeting_summary
-
-    render :partial => "action_log_quick_bar/meeting_summary_content", :locals => { :meeting_summary => s}
+    begin
+      s = create_meeting_summary
+      render :partial => "action_log_quick_bar/meeting_summary_content", :locals => { :meeting_summary => s}
+    rescure Exception exc
+      # nop
+    else
+      # nop
+    end
   end
 end
