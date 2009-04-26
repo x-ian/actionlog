2e9bc54b427a69092e98b136757c15e859aa6415
U-CHRIS\neumann <neumann@chris.(none)>
2009-04-26 09:45:24 +0200

    meeting summary update deactivated

--- app/controllers/action_log_quick_bar_controller.rb
+++ app/controllers/action_log_quick_bar_controller.rb
@@ -3,13 +3,7 @@ class ActionLogQuickBarController < ApplicationController
   before_filter :login_required
 
   def update_meeting_summary
-    begin
-      s = create_meeting_summary
-      #render :partial => "action_log_quick_bar/meeting_summary_content", :locals => { :meeting_summary => s}
-    rescure Exception exc
-      # nop
-    else
-      # nop
-    end
+    s = create_meeting_summary
+    render :partial => "action_log_quick_bar/meeting_summary_content", :locals => { :meeting_summary => s}
   end
 end
