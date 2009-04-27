e5fef504896ecd1202ec0ba386a6a343e54d0ada
U-CHRIS\neumann <neumann@chris.(none)>
2009-04-27 13:15:01 +0200

    ...

--- app/controllers/action_log_quick_bar_controller.rb
+++ app/controllers/action_log_quick_bar_controller.rb
@@ -2,6 +2,8 @@ class ActionLogQuickBarController < ApplicationController
 
   before_filter :login_required
 
+  protect_from_forgery :except => :update_meeting_summary
+
   def update_meeting_summary
     s = create_meeting_summary
     render :partial => "action_log_quick_bar/meeting_summary_content", :locals => { :meeting_summary => s}
