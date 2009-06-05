39a3d4dfbce303ecd3c68cc229a2548a32856a4e
x-ian <x-ian@x-ian.net>
2009-06-04 21:38:25 -0500

    alte scaffolds and ueberfluessige tests raus

---
+++ test/functional/customization/action_priority_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class Customization::ActionPriorityControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
